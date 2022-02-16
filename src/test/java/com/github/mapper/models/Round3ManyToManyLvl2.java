e7a09d098cb3f0fe95591b4de76c642839668b59
hellcrow <bakamakafoqq@gmail.com>
2022-02-16 23:14:00 +0200

    rename entity

--- src/test/java/com/github/mapper/models/Round3ManyToManyLvl2.java
+++ src/test/java/com/github/mapper/models/Round3ManyToManyLvl2.java
@@ -8,22 +8,22 @@ public class Round3ManyToManyLvl2 {
 
     private Long id;
 
-    private List<Round4ManyToManyLvl2> round4ManyToManyLvl2s = new ArrayList<>();
+    private List<Round4ManyToManyLvl3> round4ManyToManyLvl3s = new ArrayList<>();
 
     public Round3ManyToManyLvl2() {
     }
 
-    public Round3ManyToManyLvl2(Long id, List<Round4ManyToManyLvl2> round4ManyToManyLvl2s) {
+    public Round3ManyToManyLvl2(Long id, List<Round4ManyToManyLvl3> round4ManyToManyLvl3s) {
         this.id = id;
-        this.round4ManyToManyLvl2s = round4ManyToManyLvl2s;
+        this.round4ManyToManyLvl3s = round4ManyToManyLvl3s;
     }
 
     public Long getId() {
         return id;
     }
 
-    public List<Round4ManyToManyLvl2> getRound4ManyToManyLvl1s() {
-        return round4ManyToManyLvl2s;
+    public List<Round4ManyToManyLvl3> getRound4ManyToManyLvl1s() {
+        return round4ManyToManyLvl3s;
     }
 
     @Override
@@ -31,19 +31,19 @@ public class Round3ManyToManyLvl2 {
         if (this == o) return true;
         if (!(o instanceof Round3ManyToManyLvl2)) return false;
         Round3ManyToManyLvl2 that = (Round3ManyToManyLvl2) o;
-        return Objects.equals(id, that.id) && Objects.equals(round4ManyToManyLvl2s, that.round4ManyToManyLvl2s);
+        return Objects.equals(id, that.id) && Objects.equals(round4ManyToManyLvl3s, that.round4ManyToManyLvl3s);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, round4ManyToManyLvl2s);
+        return Objects.hash(id, round4ManyToManyLvl3s);
     }
 
     @Override
     public String toString() {
         return "Round3ManyToManyLvl1{" +
                 "id=" + id +
-                ", round4ManyToManyLvl1s=" + round4ManyToManyLvl2s +
+                ", round4ManyToManyLvl1s=" + round4ManyToManyLvl3s +
                 '}';
     }
 }
--- src/test/java/com/github/mapper/models/Round4ManyToManyLvl3.java
+++ src/test/java/com/github/mapper/models/Round4ManyToManyLvl3.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Round4ManyToManyLvl2 {
+public class Round4ManyToManyLvl3 {
 
     private Long id;
 
     private List<Round3ManyToManyLvl2> round3ManyToManyLvl2s = new ArrayList<>();
 
-    public Round4ManyToManyLvl2() {
+    public Round4ManyToManyLvl3() {
     }
 
-    public Round4ManyToManyLvl2(Long id, List<Round3ManyToManyLvl2> round3ManyToManyLvl2s) {
+    public Round4ManyToManyLvl3(Long id, List<Round3ManyToManyLvl2> round3ManyToManyLvl2s) {
         this.id = id;
         this.round3ManyToManyLvl2s = round3ManyToManyLvl2s;
     }
@@ -29,8 +29,8 @@ public class Round4ManyToManyLvl2 {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Round4ManyToManyLvl2)) return false;
-        Round4ManyToManyLvl2 that = (Round4ManyToManyLvl2) o;
+        if (!(o instanceof Round4ManyToManyLvl3)) return false;
+        Round4ManyToManyLvl3 that = (Round4ManyToManyLvl3) o;
         return Objects.equals(id, that.id) && Objects.equals(round3ManyToManyLvl2s, that.round3ManyToManyLvl2s);
     }
 
